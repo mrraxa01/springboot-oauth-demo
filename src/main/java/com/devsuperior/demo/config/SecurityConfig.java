fbb3f8fa26cc6a0ba3549fedb092dae119f1bb78
Marcio Rodrigues <mrraxa01@gmail.com>
2024-04-18 01:51:31 -0300

    Passo 3 - BCrypt

--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +17,9 @@ public class SecurityConfig {
 		return http.build();
 	}
 	
-	
+	@Bean
+	 PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$yY4ge8ue11bhN7SMMcMu2ONJmhjOM2LtxYdWg48AZd8Ay7juaPI0m');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$yY4ge8ue11bhN7SMMcMu2ONJmhjOM2LtxYdWg48AZd8Ay7juaPI0m');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
