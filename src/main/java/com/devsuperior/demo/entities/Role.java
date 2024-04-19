ce45e20cfab46da90e05b409ac09a2a15a508c09
Marcio Rodrigues <mrraxa01@gmail.com>
2024-04-18 22:53:32 -0300

    Passo 4 - 1 - Impl GrantedAuthority and UserDetail

--- src/main/java/com/devsuperior/demo/entities/Role.java
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -2,6 +2,8 @@ package com.devsuperior.demo.entities;
 
 import java.util.Objects;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,7 +13,10 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name="tb_role")
-public class Role {
+public class Role implements GrantedAuthority{
+
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -31,7 +36,8 @@ public class Role {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
+	@Override
 	public String getAuthority() {
 		return authority;
 	}
